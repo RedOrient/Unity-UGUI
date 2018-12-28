ee1e09f33ac506d42945a87444b579a3cbfab8a0
ZhuDongFang <zdf@jifangame.com>
2018-12-28 10:52:08 +0800

    First Commit

---
+++ Assets/GManager02.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GManager02 : MonoBehaviour {
+
+    public Transform GameCube;
+    public float GameCube_speed = 10;
+    public void OnSliderRotation(float value)
+    {
+        GameCube_speed = value;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameCube.Rotate(Vector3.up * Time.timeScale * GameCube_speed);
+    }
+}
